1d8e14b7c77d9b4cdb5c8066fcd4fbfab1bbe200
Francisco Castellanos <xacfran@gmail.com>
2024-03-07 01:32:21 -0600

    updated tutorials
    Added several lines of code to the telemetry tutorial. changed common.R, added and removed figures

--- _common.R
+++ _common.R
@@ -1,16 +1,21 @@
 
 # Libraries for all scripts
+## Intro
 library(tidyverse)
 library(rmarkdown)
 library(knitr)
 library(rafalib)
 library(ggExtra)
+## Analisis de datos
 library(factoextra)
 library(ggfortify)
+## Telemetria
+library(sp)
+library(move2)
 
 # Set global options for knitr
 knitr::opts_chunk$set(
-    collapse = TRUE,
-    warning = FALSE,
-    message = FALSE
+  collapse = TRUE,
+  warning = FALSE,
+  message = FALSE
 )
